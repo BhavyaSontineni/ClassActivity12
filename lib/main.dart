48720391d02b6cbac99d859ed4b7faa8377ba578
BhavyaSontineni <bsontineni1@student.gsu.edu>
2025-04-02 14:29:19 -0400

    inventory mgmt app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+//Bhavya Sri Sai-002893685
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
